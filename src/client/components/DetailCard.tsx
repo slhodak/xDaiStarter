396c1081ae155a49cbcedd51e6c3db3a820b148d
Sam Hodak <slhodak@gmail.com>
2021-03-27 15:46:32 -0700

    Laying out detail pool info

--- src/client/components/DetailCard.tsx
+++ src/client/components/DetailCard.tsx
@@ -1,5 +1,6 @@
 import Header from './Header';
 import Disclaimer from './Disclaimer';
+import DetailInfoBlock from './DetailInfoBlock';
 
 export default (props: { selectPage: Function }) => {
   const { selectPage } = props;
@@ -10,44 +11,46 @@ export default (props: { selectPage: Function }) => {
     { title: 'Liquidity Allocation', value: 100, unit: '%' },{ title: 'Liquidity Lock Duration', value: 90, unit: 'Days' }
   ];
   return (
-    <div className="detail_card">
+    <div>
       <Header selectPage={selectPage} />
-      <div className="pool_detail">
-        <div className="pool_detail_top">
-          <div className="pool_detail_title">
-          <img src="img/blockIcn.png" alt="icn"/>
-            <h4>Blockchain cuties</h4>
-            <p>asdfasdfasdfasdf</p>
-          </div>
-          <button className="btn">
-            Open in 5 Days
-          </button>
-        </div>
-        <div className="pool_detail_middle">
-          {sampleDetails}
-        </div>
-        <div className="pool_detail_bottom">
-          <div className="pool_detail_progress">
-            <div className="progress_total">
-              <p className="progress_total_title">Total Raise</p>
-              <p className="progress_total_value">$330.000 XDAI Raised</p>
+      <div className="detail_card">
+        <div className="pool_detail">
+          <div className="pool_detail_top">
+            <div className="pool_detail_top_left">
+              <img src="img/blockIcn.png" alt="icn"/>
+              <div className="pool_detail_title">
+                <h4>Blockchain cuties</h4>
+                <p>asdfasdfasdfasdf</p>
+              </div>
             </div>
-            <div className="progress_participants">
-              0 Participants
+            <button className="btn">Open in 5 Days</button>
+          </div>
+          <div className="pool_detail_middle">
+            {sampleDetails.map((info, index) => <DetailInfoBlock info={info} index={index} totalBlocks={sampleDetails.length} />)}
+          </div>
+          <div className="pool_detail_bottom">
+            <div className="pool_detail_progress">
+              <div className="progress_total">
+                <p className="progress_total_title">Total Raise</p>
+                <p className="progress_total_value">$330.000 XDAI Raised</p>
+              </div>
+              <div className="progress_participants">
+                0 Participants
+              </div>
+            </div>
+            <div className="progress_bar">
+              progress bar
+            </div>
+            <div className="progress_percent_fraction">
+              <p className="percent_value">20.0%</p>
+              <p className="fraction_value">0.00/2500.00XDAI</p>
             </div>
           </div>
-          <div className="progress_bar">
-            progress bar
-          </div>
-          <div className="progress_percent_fraction">
-            <p className="percent_value">20.0%</p>
-            <p className="fraction_value">0.00/2500.00XDAI</p>
-          </div>
         </div>
+        <div className="your_investment"></div>
+        <div className="important_links"></div>
+        <Disclaimer />
       </div>
-      <div className="your_investment"></div>
-      <div className="important_links"></div>
-      <Disclaimer />
     </div>
   );
 }
--- src/client/components/DetailInfoBlock.tsx
+++ src/client/components/DetailInfoBlock.tsx
@@ -1,9 +1,11 @@
-export default (props: { info: { title: string, value: string } }) => {
-  const { info } = props;
+export default (props: { info: { title: string, value: number, unit: string }, index: number, totalBlocks: number }) => {
+  const { info, index, totalBlocks } = props;
+  const ibr = (index + 1) % 4 != 0;
+  const ibb = index < totalBlocks - 4;
   return (
-    <div className="pool_detail_info_block">
+    <div className={`pool_detail_info_block ${ibb ? 'light_border_bottom' : ''} ${ibr ? 'light_border_right' : ''}`}>
       <p className="detail_block_title">{info.title}</p>
-      <p className="detail_block_value">{info.value}</p>
+      <p className="detail_block_value">{info.value} {info.unit}</p>
     </div>
   )
 
