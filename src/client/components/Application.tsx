d54a476d8bec22f0d768db5d72e4e3d1e556f97f
Sam Hodak <slhodak@gmail.com>
2021-03-31 22:00:00 -0700

    Application scss refactor

--- src/client/components/Application.tsx
+++ src/client/components/Application.tsx
@@ -6,161 +6,145 @@ export default class Application extends React.Component<{}, {}> {
   constructor(props: any) {
     super(props);
   }
-
   componentDidMount() {
     ($('select') as any).niceSelect();
   }
 
   render() {
     return (
-      <section className="wrap-row">
+      <section className="xdaistarter_apply">
         <aside className="aside">
-          <div className="aside_decor">
-            <img src="img/rectangle.png" alt="img"/>
+          <div className="aside_header">
+            <div className="aside_decor">
+              <img src="img/rectangle.png" alt="img"/>
+            </div>
+            <h2 className="apply_title">New Application</h2>
+            <p className="apply_descriptor">Moments away from the xDaiStarter</p>
           </div>
-          <h2 className="apply_main_title">New Application</h2>
-          <p className="apply_main_title_descr">Moments away from the xdaistarter</p>
-          <ul className="aside_btns_wrap tabs">
-            <li className="active">
-                <div className="aside_btn">
-                  <p className="apply_title_descr">Step 1</p>
-                  <h2 className="apply_title">Create Application</h2>
-                </div>
+          <ul className="aside_btns_wrap">
+            <li>
+              <div className="aside_btn">
+                <p className="apply_descriptor">Step 1</p>
+                <h2 className="apply_title">Create Application</h2>
+              </div>
             </li>
             <li>
-                <div className="aside_btn">
-                  <p className="apply_title_descr">Step 2</p>
-                  <h2 className="apply_title">XDaiStarter Team Review</h2>
-                </div>
+              <div className="aside_btn">
+                <p className="apply_descriptor">Step 2</p>
+                <h2 className="apply_title">XDaiStarter Team Review</h2>
+              </div>
             </li>
             <li>
-                <div className="aside_btn">
-                  <p className="apply_title_descr">Step 3</p>
-                  <h2 className="apply_title">Revision</h2>
-                </div>
+              <div className="aside_btn">
+                <p className="apply_descriptor">Step 3</p>
+                <h2 className="apply_title">Revision</h2>
+              </div>
             </li>
             <li>
-                <div className="aside_btn">
-                  <p className="apply_title_descr">Step 4</p>
-                  <h2 className="apply_title">XDaiStarter Council Review</h2>
-                </div>
+              <div className="aside_btn">
+                <p className="apply_descriptor">Step 4</p>
+                <h2 className="apply_title">XDaiStarter Council Review</h2>
+              </div>
             </li>
             <li>
-                <div className="aside_btn">
-                  <p className="apply_title_descr">Step 5</p>
-                  <h2 className="apply_title">Decisions and Next Steps</h2>
-                </div>
+              <div className="aside_btn">
+                <p className="apply_descriptor">Step 5</p>
+                <h2 className="apply_title">Decisions and Next Steps</h2>
+              </div>
             </li>
           </ul>
         </aside>
-        <section className="forms_content">
-          <div className="forms_header">
+        <section className="apply_main">
+          <div className="apply_main_header">
             <h2>Application<span>/ New application</span></h2>
           </div>
-          <div className="forms_content-wrap">
-            <div className="tabs-panel active" data-index="0">
-              <h2 className="apply_title">Contact information</h2>
-              <p className="apply_title_descr">We currently have the following contact information if we want to get in touch with you. Feel free to change it in user </p>
-              <div className="forms_info">
-                <h4>Contact Name: </h4><span>Alex</span>
+          <div className="apply_form">
+            <div data-index="0">
+              <div className="applicant_info_box">
+                <h2 className="apply_title">Contact information</h2>
+                <p>We currently have the following contact information if we want to get in touch with you. Feel free to change it in user </p>
               </div>
-              <div className="forms_info">
-                <h4>Contact Telegram: </h4><span>@telegram</span>
+              <div className="applicant_info">
+                <h4>Contact Name:</h4>
+                <span>Alex</span>
               </div>
-              <div className="forms_info">
-                <h4>Contact Email: </h4><span>mail#mail.com</span>
+              <div className="applicant_info">
+                <h4>Contact Telegram:</h4>
+                <span>@telegram</span>
               </div>
-              <h2 className="apply_title apply_title_form ">Basic information</h2>
-              <form action="">
-                <label htmlFor="">
-                  <p>Project Name </p>
+              <div className="applicant_info">
+                <h4>Contact Email:</h4>
+                <span>mail#mail.com</span>
+              </div>
+              <div className="application_inputs">
+                <h2 className="apply_title">Basic information</h2>
+                <div className="labeled_input">
+                  <label htmlFor="">Project Name</label>
                   <input type="text" placeholder="Project Name "/>
-                </label>
-                <label htmlFor="">
-                  <p>Website URL </p>
+                </div>
+                <div className="labeled_input">
+                  <label htmlFor="">Website URL</label>
                   <input type="text" placeholder="Website URL "/>
-                </label>
-                <label htmlFor="">
-                  <p>White paper </p>
+                </div>
+                <div className="labeled_input">
+                  <label htmlFor="">White paper</label>
                   <input type="text" placeholder="White paper "/>
-                </label>
-                <label htmlFor="">
-                  <p>Telegram Handle </p>
+                </div>
+                <div className="labeled_input">
+                  <label htmlFor="">Telegram Handle</label>
                   <input type="text" placeholder="Project Name "/>
-                </label>
-                <label className="textarea" htmlFor="">
-                  <p>Telegram Handle </p>
-                  <textarea name="" id=""
-                    placeholder="Token distribution, token sale metrics etc..."></textarea>
-                </label>
-                <label className="textarea" htmlFor="">
-                  <p>Description </p>
+                </div>
+                <div className="labeled_input">
+                  <label className="textarea" htmlFor="">Telegram Handle</label>
+                  <textarea name="" id="" placeholder="Token distribution, token sale metrics etc..."></textarea>
+                </div>
+                <div className="labeled_input">
+                  <label className="textarea" htmlFor="">Description</label>
                   <textarea name="" id="" placeholder="Paper Description"></textarea>
-                </label>
-                <div className="form_disc">
-                  <p>We will use the description to showcase your project on the pool page
-                    if application approved.</p>
                 </div>
-              </form>
-              <h2 className="apply_title apply_title_form ">Founder & progress information</h2>
-              <form action="">
-
-                <label className="textarea" htmlFor="">
-                  <p>Founder Linkedins </p>
+                <p>We will use the description to showcase your project on the pool page if application approved.</p>
+              </div>
+              <div className="application_inputs">
+                <h2 className="apply_title">Founder & progress information</h2>
+                <div className="labeled_input">
+                  <label className="textarea" htmlFor="">Founder Linkedins</label>
                   <textarea name="" id="" placeholder="Linkdin URL of founders"></textarea>
-                </label>
-                <label className="textarea" htmlFor="">
-                  <p>Github URL </p>
+                </div>
+                <div className="labeled_input">
+                  <label className="textarea" htmlFor="">Github URL</label>
                   <textarea name="" id="" placeholder="Github URL"></textarea>
-                </label>
-                <label className="textarea" htmlFor="">
-                  <p>State of Development </p>
-                  <textarea name="" id="" placeholder="Any Product & MVP live? testnet?"></textarea>
-                </label>
-                <label className="textarea" htmlFor="">
-                  <p>How much of
-                    raised? </p>
-                  <textarea name="" id="" placeholder="Examole 100k Seed,500k Private sale"></textarea>
-                </label>
-              </form>
-              <h2 className="apply_title apply_title_form ">Founder & progress information</h2>
-              <form action="">
-                <label htmlFor="" className="col">
-                  <p>How much are you looking to raise with us? </p>
+                  </div>
+                <div className="labeled_input">
+                  <label className="textarea" htmlFor="">State of Development</label>
+                  <textarea name="" id="" placeholder="Any Product & MVP live? Testnet?"></textarea>
+                </div>
+                <div className="labeled_input">
+                  <label className="textarea" htmlFor="">How much of raised?</label>
+                  <textarea name="" id="" placeholder="Example 100k Seed, 500k Private sale"></textarea>
+                </div>
+              </div>
+              <div className="application_inputs">
+                <h2 className="apply_title">Founder & progress information</h2>
+                <div className="column_input">
+                  <p>How much are you looking to raise with us?</p>
                   <input type="text" placeholder="eg:1000000 "/>
-                </label>
-
-                <div className="form_disc2">
-                  <p>How much are you looking to raise with us? </p>
                 </div>
-                <div className="form_date">
-                  <label htmlFor="Year">
+                <div className="column_input">
+                  <p>When would you like to launch?</p>
+                  <div className="form_date">
+                    <label htmlFor="Year"></label>
                     <input type="number" value="2021" onChange={() => {}} />
-                  </label>
-                  <label htmlFor="Month">
-                    <select className="input_select">
-                      <option value={0}>January</option>
-                      <option value={1}>February</option>
-                      <option value={2}>March</option>
-                      <option value={3}>April</option>
-                      <option value={4}>May</option>
-                      <option value={5}>June</option>
-                      <option value={6}>July</option>
-                      <option value={7}>August</option>
-                      <option value={8}>September</option>
-                      <option value={9}>October</option>
-                      <option value={10}>November</option>
-                      <option value={11}>December</option>
-                    </select>
-                  </label>
-                  <label htmlFor="Day">
+                    <label htmlFor="Month"></label>
                     <select className="input_select">
                     </select>
-                  </label>
+                    <label htmlFor="Day"></label>
+                    <select className="input_select">
+                    </select>
+                  </div>
                 </div>
-              </form>
-              <div className="form_btn-wrap">
-                <button className="btn">Submit Application</button>
+                <div className="form_btn-wrap">
+                  <button className="btn">Submit Application</button>
+                </div>
               </div>
             </div>
           </div>
