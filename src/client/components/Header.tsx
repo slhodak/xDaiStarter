b71fdcafb2323712c4f4732c1579a630c77bed4e
Sam Hodak <slhodak@gmail.com>
2021-04-19 16:32:40 -0700

    Wallet connect (#3)
    * Add wallet connection with web3modal
    
    * Enable wallet connection; include Torus to demo modal
    
    * Remove some logs and comments

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -1,7 +1,18 @@
 import { Link } from 'react-router-dom';
+import { Wallet, useWeb3Modal } from './Wallet';
 import '../img/l2.png';
 
+const INFURA_ID = '026977bc41064333a6c70afe674a9b99';
+
+const NETWORK_NAME = 'kovan';
+
+const web3ModalOptions = {
+  autoLoad: true, infuraId: INFURA_ID, NETWORK: NETWORK_NAME
+};
+
 export default (props: any) => {
+  const { provider, account, toggleWeb3Modal } = useWeb3Modal(web3ModalOptions);
+
   return (
     <header className="header_container">
       <div className="header_background">
@@ -13,7 +24,7 @@ export default (props: any) => {
             <a href="#">Pools</a>
             <a href="#">Stake</a>
             <Link to="/apply">Create Pool</Link>
-            <button>Connect Wallet</button>
+            <Wallet provider={provider} account={account} toggleWeb3Modal={toggleWeb3Modal} />
           </div>
         </div>
       </div>
---
+++ src/client/components/Wallet.tsx
@@ -0,0 +1,87 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Web3Provider } from '@ethersproject/providers';
+import Web3Modal from 'web3modal';
+import Torus from '@toruslabs/torus-embed';
+
+type UseWeb3ModalConfig = {
+  autoLoad: boolean,
+  infuraId: string,
+  NETWORK: string
+};
+
+const useWeb3Modal = function({ autoLoad, infuraId, NETWORK }: UseWeb3ModalConfig) {
+  const [provider, setProvider] = useState<Web3Provider>();
+  const [account, setAccount] = useState<string>();
+
+  const web3Modal = new Web3Modal({
+    network: NETWORK,
+    cacheProvider: true,
+    // Metamask available by default
+    providerOptions: {
+      torus: {
+        package: Torus,
+        options: {
+          // networkParams: {
+          //   host: "https://localhost:8545", // optional
+          //   chainId: 1337, // optional
+          //   networkId: 1337 // optional
+          // },
+          config: {
+            buildEnv: "development" // optional
+          }
+        }
+      }
+    }
+  });
+
+  const addressDisplayed = (address: string) => {
+    return address.slice(0, 6) + "..." + address.slice(-4)
+  };
+
+  // Open wallet selection modal
+  const loadWeb3Modal = useCallback(async () => {
+    const newProvider = await web3Modal.connect();
+    const provider = new Web3Provider(newProvider);
+    console.debug('Connecting to provider', provider);
+    setProvider(provider);
+    const accounts = await provider.listAccounts();
+    setAccount(addressDisplayed(accounts[0]));
+  }, [web3Modal]);
+
+  const logoutOfWeb3Modal = useCallback(async () => {
+    await web3Modal.clearCachedProvider();
+    setProvider(undefined);
+  }, [web3Modal]);
+
+  useEffect(() => {
+    if (autoLoad && !provider && web3Modal.cachedProvider) {
+      loadWeb3Modal();
+    }
+  });
+
+  const toggleWeb3Modal = () => {
+    if (provider) {
+      logoutOfWeb3Modal();
+    } else {
+      loadWeb3Modal();
+    }
+  }
+
+  return { provider, account, toggleWeb3Modal };
+}
+
+type WalletConfig = {
+  provider?: Web3Provider,
+  account?: string,
+  toggleWeb3Modal: Function
+}
+
+function Wallet({ provider, account, toggleWeb3Modal}: WalletConfig) {
+  return (
+    <button onClick={() => toggleWeb3Modal()}>
+      {provider ? account : 'Connect Wallet'}
+    </button>
+  );
+}
+
+export { Wallet, useWeb3Modal };
