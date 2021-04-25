ef13fcc1535429ff04b7e2ef4f64260d35370356
Sam Hodak <slhodak@gmail.com>
2021-04-25 16:01:58 -0700

    Write first XDPToken tests

--- test/contracts/XDPToken.js
+++ test/contracts/XDPToken.js
@@ -1,8 +1,17 @@
+const assert = require("chai").assert;
 const XDPToken = artifacts.require("XDPToken");
 
+before(async () => {
+  xdpToken = await XDPToken.deployed();
+});
+
 contract("XDPToken", async accounts => {
-  it("should be a token", async () => {
-    const instance = await XDPToken.deployed();
-    assert(true);
+  it("should be named", async () => {
+    const name = await xdpToken.name();
+    assert.equal(name, "xDaiStarter");
+  });
+  it("should have a symbol", async() => {
+    const symbol = await xdpToken.symbol();
+    assert.equal(symbol, "XDP");
   });
 });
