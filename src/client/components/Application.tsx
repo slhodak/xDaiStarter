8d2ad63112c95d30f5fbeb9c1ca8ffa35b056431
slhodak <slhodak@gmail.com>
2021-06-03 15:39:59 -0700

    Convert contact info area into submittable fields

--- src/client/components/Application.tsx
+++ src/client/components/Application.tsx
@@ -60,140 +60,135 @@ export default (props: any) => {
           <h2>Application<span>/ New application</span></h2>
         </div>
         <form action="/application" method="post" className="apply_form">
-          <div data-index="0">
-            <div className="applicant_info_header">
-              <h2 className="medium_title">Contact information</h2>
-              <p className="medium_descriptor">We currently have the following contact information if we want to get in touch with you. Feel free to change it in user </p>
+          <div className="application_inputs">
+            <h2 className="medium_title">Contact Information</h2>
+            <div className="labeled_input">
+              <label htmlFor="contactName">Name</label>
+              <input name="contactName" type="text" placeholder="Full Name"/>
             </div>
-            <div className="applicant_info">
-              <h4>Contact Name:</h4>
-              <span>Alex</span>
+            <div className="labeled_input">
+              <label htmlFor="contactSocial">Social Media</label>
+              <input name="contactSocial" type="text" placeholder="@founder"/>
             </div>
-            <div className="applicant_info">
-              <h4>Contact Telegram:</h4>
-              <span>@telegram</span>
+            <div className="labeled_input">
+              <label htmlFor="contactEmail">Email</label>
+              <input name="contactEmail" type="text" placeholder="Project Name"/>
             </div>
-            <div className="applicant_info">
-              <h4>Contact Email:</h4>
-              <span>mail#mail.com</span>
+            <h2 className="medium_title">Project Information</h2>
+            <div className="labeled_input">
+              <label htmlFor="projectName">Project Name</label>
+              <input name="projectName" type="text" placeholder="Project Name"/>
             </div>
-            <div className="application_inputs">
-              <h2 className="medium_title">Basic Information</h2>
-              <div className="labeled_input">
-                <label htmlFor="projectName">Project Name</label>
-                <input name="projectName" type="text" placeholder="Project Name"/>
-              </div>
-              <div className="labeled_input">
-                <label htmlFor="websiteURL">Website URL</label>
-                <input name="websiteURL" type="text" placeholder="Website URL"/>
-              </div>
-              <div className="labeled_input">
-                <label htmlFor="whitepaper">Whitepaper</label>
-                <input name="whitepaper" type="text" placeholder="Whitepaper URL"/>
-              </div>
-              <div className="labeled_input">
-                <label htmlFor="socialHandle">Social Media Handle</label>
-                <input name="socialHandle" type="text" placeholder="@us"/>
-              </div>
-              <div className="labeled_input">
-                <label className="textarea" htmlFor="description">Description</label>
-                <textarea name="description" placeholder="Paper Description"></textarea>
-              </div>
-              <p>We will use the description to showcase your project on the pool page if the application is approved.</p>
+            <div className="labeled_input">
+              <label htmlFor="websiteURL">Website URL</label>
+              <input name="websiteURL" type="text" placeholder="Website URL"/>
             </div>
-            <div className="application_inputs">
-              <h2 className="medium_title">Founder & Progress Information</h2>
-              <div className="labeled_input">
-                <label className="textarea" htmlFor="founders">Founder Profiles</label>
-                <p className="">Please separate by a comma</p>
-                <textarea name="founders" placeholder="Professional Bios, Personal Sites, LinkedIn URLs, etc, of founders"></textarea>
-              </div>
-              <div className="labeled_input">
-                <label className="textarea" htmlFor="github">Github URL</label>
-                <textarea name="github" placeholder="Github URL"></textarea>
-                </div>
-              <div className="labeled_input">
-                <label className="textarea" htmlFor="stateOfDevelopment">State of Development</label>
-                <textarea name="stateOfDevelopment" placeholder="Any Product & MVP live? Testnet?"></textarea>
-              </div>
-              <div className="labeled_input">
-                <label className="textarea" htmlFor="amountRaised">How much raised so far?</label>
-                <textarea name="amountRaised" placeholder="Example 100k Seed, 500k Private sale"></textarea>
-              </div>
+            <div className="labeled_input">
+              <label htmlFor="whitepaper">Whitepaper</label>
+              <input name="whitepaper" type="text" placeholder="Whitepaper URL"/>
             </div>
-            <div className="application_inputs">
-              <h2 className="medium_title">Capital & Timeline</h2>
-              <div className="column_inputs">
-                <div className="column_input">
-                  <p className="medium_descriptor">How much are you looking to raise with us?</p>
-                  <input name="hardcap" type="text" placeholder="eg:1000000 "/>
-                </div>
-                <div className="column_input">
-                  <p className="medium_descriptor">When would you like to launch?</p>
-                  <div className="form_date">
-                    <div className="date_input">
-                      <label htmlFor="year"></label>
-                      <input name="year" type="number" value="2021" />
-                    </div>
-                    <div className="date_input">
-                      <label htmlFor="month"></label>
-                      <select name="month" className="input_select">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                      </select>
-                    </div>
-                    <div className="date_input">
-                      <label htmlFor="day">
-                      </label>
-                      <select name="day" className="input_select">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        <option value="25">25</option>
-                        <option value="26">26</option>
-                        <option value="27">27</option>
-                        <option value="28">28</option>
-                        <option value="29">29</option>
-                        <option value="30">30</option>
-                        <option value="31">31</option>
-                      </select>
-                    </div>
+            <div className="labeled_input">
+              <label htmlFor="socialHandle">Social Media Handle</label>
+              <input name="socialHandle" type="text" placeholder="@us"/>
+            </div>
+            <div className="labeled_input">
+              <label className="textarea" htmlFor="description">Description</label>
+              <textarea name="description" placeholder="Paper Description"></textarea>
+            </div>
+            <p>We will use the description to showcase your project on the pool page if the application is approved.</p>
+          </div>
+          <div className="application_inputs">
+            <h2 className="medium_title">Founder & Progress Information</h2>
+            <div className="labeled_input">
+              <label className="textarea" htmlFor="founders">Founder Profiles</label>
+              <p className="">Please separate by a comma</p>
+              <textarea name="founders" placeholder="Professional Bios, Personal Sites, LinkedIn URLs, etc, of founders"></textarea>
+            </div>
+            <div className="labeled_input">
+              <label className="textarea" htmlFor="github">Github URL</label>
+              <textarea name="github" placeholder="Github URL"></textarea>
+              </div>
+            <div className="labeled_input">
+              <label className="textarea" htmlFor="stateOfDevelopment">State of Development</label>
+              <textarea name="stateOfDevelopment" placeholder="Any Product & MVP live? Testnet?"></textarea>
+            </div>
+            <div className="labeled_input">
+              <label className="textarea" htmlFor="amountRaised">How much raised so far?</label>
+              <textarea name="amountRaised" placeholder="Example 100k Seed, 500k Private sale"></textarea>
+            </div>
+          </div>
+          <div className="application_inputs">
+            <h2 className="medium_title">Capital & Timeline</h2>
+            <div className="column_inputs">
+              <div className="column_input">
+                <p className="medium_descriptor">How much are you looking to raise with us?</p>
+                <input name="hardcap" type="text" placeholder="eg:1000000 "/>
+              </div>
+              <div className="column_input">
+                <p className="medium_descriptor">When would you like to launch?</p>
+                <div className="form_date">
+                  <div className="date_input">
+                    <label htmlFor="year"></label>
+                    <input name="year" type="number" value="2021" />
+                  </div>
+                  <div className="date_input">
+                    <label htmlFor="month"></label>
+                    <select name="month" className="input_select">
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="11">11</option>
+                      <option value="12">12</option>
+                    </select>
+                  </div>
+                  <div className="date_input">
+                    <label htmlFor="day">
+                    </label>
+                    <select name="day" className="input_select">
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                      <option value="11">11</option>
+                      <option value="12">12</option>
+                      <option value="13">13</option>
+                      <option value="14">14</option>
+                      <option value="15">15</option>
+                      <option value="16">16</option>
+                      <option value="17">17</option>
+                      <option value="18">18</option>
+                      <option value="19">19</option>
+                      <option value="20">20</option>
+                      <option value="21">21</option>
+                      <option value="22">22</option>
+                      <option value="23">23</option>
+                      <option value="24">24</option>
+                      <option value="25">25</option>
+                      <option value="26">26</option>
+                      <option value="27">27</option>
+                      <option value="28">28</option>
+                      <option value="29">29</option>
+                      <option value="30">30</option>
+                      <option value="31">31</option>
+                    </select>
                   </div>
                 </div>
               </div>
-              <input className="form_btn" type="submit" value="Submit Application"></input>
             </div>
+            <input className="form_btn" type="submit" value="Submit Application"></input>
           </div>
         </form>
       </section>
