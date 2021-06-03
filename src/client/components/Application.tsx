f174e65dff623edbcfc6bdf63919d1b7ae8b497a
slhodak <slhodak@gmail.com>
2021-06-03 15:59:00 -0700

    Swap date selection elements for single date picker

--- src/client/components/Application.tsx
+++ src/client/components/Application.tsx
@@ -100,8 +100,10 @@ export default (props: any) => {
           <div className="application_inputs">
             <h2 className="medium_title">Founder & Progress Information</h2>
             <div className="labeled_input">
-              <label className="textarea" htmlFor="founders">Founder Profiles</label>
-              <p className="">Please separate by a comma</p>
+              <div>
+                <label className="textarea" htmlFor="founders">Founder Profiles</label>
+                <p>Please separate by a comma</p>
+              </div>
               <textarea name="founders" placeholder="Professional Bios, Personal Sites, LinkedIn URLs, etc, of founders"></textarea>
             </div>
             <div className="labeled_input">
@@ -126,66 +128,8 @@ export default (props: any) => {
               </div>
               <div className="column_input">
                 <p className="medium_descriptor">When would you like to launch?</p>
-                <div className="form_date">
-                  <div className="date_input">
-                    <label htmlFor="year"></label>
-                    <input name="year" type="number" value="2021" />
-                  </div>
-                  <div className="date_input">
-                    <label htmlFor="month"></label>
-                    <select name="month" className="input_select">
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
-                      <option value="6">6</option>
-                      <option value="7">7</option>
-                      <option value="8">8</option>
-                      <option value="9">9</option>
-                      <option value="10">10</option>
-                      <option value="11">11</option>
-                      <option value="12">12</option>
-                    </select>
-                  </div>
-                  <div className="date_input">
-                    <label htmlFor="day">
-                    </label>
-                    <select name="day" className="input_select">
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
-                      <option value="6">6</option>
-                      <option value="7">7</option>
-                      <option value="8">8</option>
-                      <option value="9">9</option>
-                      <option value="10">10</option>
-                      <option value="11">11</option>
-                      <option value="12">12</option>
-                      <option value="13">13</option>
-                      <option value="14">14</option>
-                      <option value="15">15</option>
-                      <option value="16">16</option>
-                      <option value="17">17</option>
-                      <option value="18">18</option>
-                      <option value="19">19</option>
-                      <option value="20">20</option>
-                      <option value="21">21</option>
-                      <option value="22">22</option>
-                      <option value="23">23</option>
-                      <option value="24">24</option>
-                      <option value="25">25</option>
-                      <option value="26">26</option>
-                      <option value="27">27</option>
-                      <option value="28">28</option>
-                      <option value="29">29</option>
-                      <option value="30">30</option>
-                      <option value="31">31</option>
-                    </select>
-                  </div>
-                </div>
+                <label style={{display: "none"}} htmlFor="date">Date</label>
+                <input type="date" name="launchDate"></input>
               </div>
             </div>
             <input className="form_btn" type="submit" value="Submit Application"></input>
